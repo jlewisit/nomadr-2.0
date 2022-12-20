816afc7147e9648554dce5fc011cbc33a4eeb8a0
jlewisit <joelewis.mortgage@gmail.com>
2022-12-20 06:06:19 -0800

    Login.js replaced

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -1,82 +1,94 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
- const Login = (props) =>{
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+import Auth from '../utils/auth';
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
-    return(
 
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
 
-        <div className = "Home">
-        <br></br>
-      <h1>A New Sky, A New Life.</h1>
-     
-      <div className = "grid-container1">
-        <div className = "grid-item1">
-      <img src = "https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city_16x9.JPG " alt= "Mexico"></img>
-      </div>
-      <div className = "grid-item1">
-    <img src = "https://www.state.gov/wp-content/uploads/2018/11/France-1980x1406.jpg" alt ="France"></img>
-    </div>
-    <div className = "grid-item1">
-    <img src = "https://i.natgeofe.com/k/33e48abd-f2e7-4430-b7bf-cc9a18c14cc6/brazil-christ-the-redeemer_2x1.jpg" alt = "Brazil"></img>
-    </div>
-    <div className = "grid-item1">
-    <img src = "https://www.state.gov/wp-content/uploads/2018/11/Spain-2109x1406.jpg" alt = "Spain"></img>
-   </div>
-   <div className = "grid-item1">
-    <img src = "https://www.travelandleisure.com/thmb/CwASoPDthL_sRQW6yA-qh-zJPlg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/santorini-POINTSGREECE0617-8cd08e70fec4471b8827dca8627b8eae.jpg" alt = "greece"></img>
-   </div>
-   <div className = "grid-item1">
-    <img src =  "https://images.ireland.com/media/Images/Tipperary/aafe8401d6a1418cb65e2767f330c94b.jpg?w=1934" alt = "Ireland"></img>
-   </div>
-   <div className = "grid-item1">
-    <img src =  "https://i.natgeofe.com/k/42e832f5-fd48-43ff-b338-091bdf4048ca/india-tajmahal_16x9.jpg?w=1200" alt = "India"></img>
-    </div>
-    <div className = "grid-item1">
-    <img src = "https://a.cdn-hotels.com/gdcs/production5/d1996/54fdb73f-eee5-4612-a3e7-6fc7ed2f7bee.jpg?impolicy=fcrop&w=800&h=533&q=medium" alt = "australia"></img>
-   </div>
-   <div className = "grid-item1">
-    <img src = "https://www.kids-world-travel-guide.com/images/santiagodechile-2.jpg" alt = "Chili"></img>
-   </div>
-   <div className = "grid-item1">
-    <img src = "https://a.cdn-hotels.com/gdcs/production159/d204/01ae3fa0-c55c-11e8-9739-0242ac110006.jpg" alt = "Canada"></img>
-  </div>
-  <div className = "grid-item1">
-      <img src = "https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city_16x9.JPG " alt= "Mexico"></img>
-      </div>
-      <div className = "grid-item1">
-    <img src = "https://www.state.gov/wp-content/uploads/2018/11/France-1980x1406.jpg" alt ="France"></img>
-    </div>
-      
-    <div className = "grid-item1">
-      <div class = "loginformcontainer1">
-      <div className="auth-form-container">
-            <h2>Login</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label><br></br>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="example@email.com" id="email" name="email" /><br></br>
-            <label htmlFor="password">Password</label><br></br>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" /><br></br>
-            <button type="submit">Log In</button><br></br>
-        
-        <button className="link-btn" onClick={()=> props.onFormSwitch('register')}>Don't have an account? Register here</button>
-        </form>
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
         </div>
+      </div>
+    </main>
+  );
+};
 
-      
-   </div>
-   </div>
-  
-    
-   
-    </div>
-   </div>
-        
-    )
-}
 export default Login;
